994aba542e658b59e4c6be431bc74c0e0a38453f
Sharif Al-Hadidi <sharifhadidi@gmail.com>
2013-07-18 14:38:54 -0700

    upload

--- db/migrate/20130711235136_create_locations.rb
+++
@@ -1,9 +0,0 @@
-class CreateLocations < ActiveRecord::Migration
-  def change
-    create_table :locations do |t|
-      t.string :city
-      t.integer :trip_id
-      t.timestamps
-    end
-  end
-end
